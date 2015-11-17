05eb96adeaa07447760490e0e3fad3cca5cbbd3b
Rube <353371737@qq.com>
2015-11-17 16:44:52 +0800

    数据

---
+++ datas/outputConfig.js
@@ -0,0 +1,17 @@
+/**
+ * @module outputConfig
+ * @author Rube
+ * @date 15/11/17
+ * @desc process.stdout.write's style
+ */
+
+const clc = require('cli-color');
+
+const out = process.stdout;
+
+module.exports = {
+    robotName: text => out.write(clc.bold.green(text)),
+    serverTypeError: type => console.log(clc.bold.red(`No server type \'${type}\' !`)),
+    commandError: command => console.log(clc.bold.red(`No command \'${command}\' !`)),
+    scriptSameWarn: scriptName => console.log(clc.bold.yellow(`Have the same name script \'${scriptName}\'`))
+};
