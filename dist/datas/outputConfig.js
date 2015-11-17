897b190a00b23cc06c2e23662db3d92b6c16fe25
Rube <353371737@qq.com>
2015-11-17 16:43:59 +0800

    统一输出配置

---
+++ dist/datas/outputConfig.js
@@ -0,0 +1,27 @@
+/**
+ * @module outputConfig
+ * @author Rube
+ * @date 15/11/17
+ * @desc process.stdout.write's style
+ */
+
+'use strict';
+
+var clc = require('cli-color');
+
+var out = process.stdout;
+
+module.exports = {
+  robotName: function robotName(text) {
+    return out.write(clc.bold.green(text));
+  },
+  serverTypeError: function serverTypeError(type) {
+    return console.log(clc.bold.red('No server type \'' + type + '\' !'));
+  },
+  commandError: function commandError(command) {
+    return console.log(clc.bold.red('No command \'' + command + '\' !'));
+  },
+  scriptSameWarn: function scriptSameWarn(scriptName) {
+    return console.log(clc.bold.yellow('Have the same name script \'' + scriptName + '\''));
+  }
+};
