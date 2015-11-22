e7561398a035681787895fea74783637f66539a5
Rube <353371737@qq.com>
2015-11-22 16:51:50 +0800

    脚本父类

---
+++ common/script.js
@@ -0,0 +1,70 @@
+/**
+ * @module Script
+ * @author Rube
+ * @date 15/11/19
+ * @desc script's method
+ */
+
+import Api from './api';
+import EmitTable from '../common/entitys/emitTable';
+import ProcessConfig from '../common/entitys/processConfig';
+
+class Script {
+
+    constructor(cxt) {
+        this.cxt = cxt;
+    }
+
+    on_process_create() {
+
+    }
+
+    on_process_start() {
+
+    }
+
+    on_process_resume() {
+
+    }
+
+    on_process_pause() {
+
+    }
+
+    on_process_stop() {
+
+    }
+
+    on_process_destroy() {
+        process.exit();
+    }
+
+    on_process_exec(funcStr, params) {
+        return this[funcStr](params);
+    }
+
+    static get_processConfig() {
+
+        let config = new ProcessConfig();
+        config.setServiceType(ProcessConfig.START_SERVICE_EVERY);
+        config.setReceiveType(ProcessConfig.RECEIVE_ONCE);
+        return config;
+    }
+
+    static get_emitTable() {
+
+        let emitTable = new EmitTable();
+        return emitTable;
+    }
+
+    static get_description() {
+        return 'no description';
+    }
+
+    static get_ask() {
+        return 'no ask';
+    }
+}
+
+export default Script;
+
---
+++ dist/common/script.js
@@ -0,0 +1,96 @@
+/**
+ * @module Script
+ * @author Rube
+ * @date 15/11/19
+ * @desc script's method
+ */
+
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var _api = require('./api');
+
+var _api2 = _interopRequireDefault(_api);
+
+var _commonEntitysEmitTable = require('../common/entitys/emitTable');
+
+var _commonEntitysEmitTable2 = _interopRequireDefault(_commonEntitysEmitTable);
+
+var _commonEntitysProcessConfig = require('../common/entitys/processConfig');
+
+var _commonEntitysProcessConfig2 = _interopRequireDefault(_commonEntitysProcessConfig);
+
+var Script = (function () {
+    function Script(cxt) {
+        _classCallCheck(this, Script);
+
+        this.cxt = cxt;
+    }
+
+    _createClass(Script, [{
+        key: 'on_process_create',
+        value: function on_process_create() {}
+    }, {
+        key: 'on_process_start',
+        value: function on_process_start() {}
+    }, {
+        key: 'on_process_resume',
+        value: function on_process_resume() {}
+    }, {
+        key: 'on_process_pause',
+        value: function on_process_pause() {}
+    }, {
+        key: 'on_process_stop',
+        value: function on_process_stop() {}
+    }, {
+        key: 'on_process_destroy',
+        value: function on_process_destroy() {
+            process.exit();
+        }
+    }, {
+        key: 'on_process_exec',
+        value: function on_process_exec(funcStr, params) {
+            return this[funcStr](params);
+        }
+    }], [{
+        key: 'get_processConfig',
+        value: function get_processConfig() {
+
+            var config = new _commonEntitysProcessConfig2['default']();
+            config.setServiceType(_commonEntitysProcessConfig2['default'].START_SERVICE_EVERY);
+            config.setReceiveType(_commonEntitysProcessConfig2['default'].RECEIVE_ONCE);
+            return config;
+        }
+    }, {
+        key: 'get_emitTable',
+        value: function get_emitTable() {
+
+            var emitTable = new _commonEntitysEmitTable2['default']();
+            return emitTable;
+        }
+    }, {
+        key: 'get_description',
+        value: function get_description() {
+            return 'no description';
+        }
+    }, {
+        key: 'get_ask',
+        value: function get_ask() {
+            return 'no ask';
+        }
+    }]);
+
+    return Script;
+})();
+
+exports['default'] = Script;
+module.exports = exports['default'];
