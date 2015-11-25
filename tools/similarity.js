36c7d62916da377934a22c4b72a34fc928dc79f9
Rube <353371737@qq.com>
2015-11-25 17:33:46 +0800

    say 命令的相似度计算

--- tools/similarity.js
+++ tools/similarity.js
@@ -1,7 +1,33 @@
 /**
- * @page
- * @module
+ * @module Similarity
  * @author Rube
  * @date 15/11/25
- * @desc
+ * @desc 相似度计算
  */
+
+export const includeSimilar = (target, source)=> {
+
+    let sum = 0;
+    source.forEach(item=> {
+
+        if (target.indexOf(item) != -1) {
+            sum++;
+        }
+    });
+    return sum / source.length;
+};
+
+export const extractSimilar = (target, source)=> {
+
+    let sum = 0;
+    let total = 0;
+    source.forEach(item=> {
+
+        let e = item.split(':');
+        if (target.indexOf(e[0]) != -1) {
+            sum += parseFloat(e[1]);
+        }
+        total += parseFloat(e[1]);
+    });
+    return sum / total;
+};
