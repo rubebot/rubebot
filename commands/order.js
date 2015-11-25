83c8b3206a4c809c657e9e703e0c3b111b171e09
Rube <353371737@qq.com>
2015-11-25 14:52:29 +0800

    order 命令初步

--- commands/order.js
+++ commands/order.js
@@ -7,6 +7,21 @@
 
 import CommandExec from '../common/entitys/commandExec';
 import * as output from '../datas/outputConfig';
+import * as bucketApi from '../common/bucketApi';
+import Command from '../common/entitys/command';
+import ProcessConfig from '../common/entitys/processConfig';
+import ProcessInfo from '../common/entitys/processInfo';
+import ExChildProcess from '../common/entitys/exchildProcess';
+import child_process from 'child_process';
+
+function splitChunk(chunk) {
+
+    let cxt = chunk.split(' ').filter(item=> {
+        return item != '';
+    });
+
+    return new Command(cxt);
+}
 
 class Order extends CommandExec {
 
@@ -36,6 +51,44 @@ class Order extends CommandExec {
                 output.scriptSameWarn(execScriptName);
             } else {
 
+                let funcCommand = splitChunk(command.nativeContent);
+                let funcStr = execScript[0].emitTable.commandEmitTable[funcCommand.main];
+                let processConfig = execScript[0].script.get_processConfig();
+
+                if (funcStr) {
+
+                    let exChildProcess = new ExChildProcess();
+                    let processInfo = new ProcessInfo();
+                    exChildProcess.setFuncStr(funcStr);
+                    exChildProcess.setScriptName(execScriptName);
+                    exChildProcess.setOption(funcCommand.options);
+
+                    if (processConfig.runType == ProcessConfig.RUN_DAEMON) {
+
+                        let p = child_process.fork(__dirname + '/../common/processDaemon.js');
+                        processInfo.setPid(p.pid);
+                        let selfpid = bucketApi.addProcess(processInfo);
+                        exChildProcess.setPid(selfpid);
+
+                        p.send({
+                            exChildProcess,
+                            processInfo                  //TODO多种运行方式,输出管理(通知和直接输出)
+                        });
+                    } else if (processConfig.runType == ProcessConfig.RUN_FAST) {
+
+                        let selfpid = bucketApi.addProcess(processInfo);
+                        exChildProcess.setPid(selfpid);
+
+                        const processFast = require('../common/processFast');
+                        processFast({
+                            exChildProcess,
+                            processInfo
+                        });
+                    }
+
+                } else {
+                    //TODO not found
+                }
             }
         } else {
             output.scriptNotFound(execScriptName);
